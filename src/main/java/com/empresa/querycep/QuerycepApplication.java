f22038666899310c018d3bba6dee897122b70572
Joan Reis <joansantosreis@gmail.com>
2025-01-14 13:15:21 -0300

    Primeiro Commit

---
+++ src/main/java/com/empresa/querycep/QuerycepApplication.java
@@ -0,0 +1,13 @@
+package com.empresa.querycep;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuerycepApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuerycepApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/empresa/querycep/QuerycepApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empresa.querycep;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuerycepApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
